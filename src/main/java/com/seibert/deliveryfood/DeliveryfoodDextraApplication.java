7f368bb9f6751c37371cc047073fc4af11f25458
seibert <marcosantonioseibert@gmail.com>
2018-08-24 21:13:29 -0300

    criando projeto Desafio dextra - Deliveryfood

---
+++ src/main/java/com/seibert/deliveryfood/DeliveryfoodDextraApplication.java
@@ -0,0 +1,12 @@
+package com.seibert.deliveryfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryfoodDextraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryfoodDextraApplication.class, args);
+	}
+}
